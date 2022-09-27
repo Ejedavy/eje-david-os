4eb8426fa0cc014d32e65753098a3a393a06e9dd
David Eje <davideje@Davids-MacBook-Pro.local>
2022-09-27 22:15:22 +0300

    Made ex4 run in background

--- week04/ex4.c
+++ week04/ex4.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
-
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
 
 int main(void) {
-  char command[50];
-  for (;;) {
+
+  pid_t pid = fork();
+  if(pid == 0){
+    char command[50];
+    for (;;) {
     scanf("%s", command);
     system(command);
   }
+  }
+  wait(NULL);
+
+  return EXIT_SUCCESS;
+  
 }
+
